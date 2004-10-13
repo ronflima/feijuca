0b385e7dd1d7fc86f6dea0fe288064a723562cca
ron_lima <ron_lima@675ac745-d94a-0410-b520-95f516b70467>
2004-10-13 00:29:12 +0000

    Changed the error reporting facilities to follow the POSIX standards Made some small esthetic changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@85 675ac745-d94a-0410-b520-95f516b70467

--- src/dlist/dlist_alloc.c
+++ src/dlist/dlist_alloc.c
@@ -2,21 +2,22 @@
  Copyright Ronaldo Faria Lima (C) 2004 - All rights reserved
  Feel free to contact the author in <ronaldo@ronaldolima.eti.br>
 
- This file is part of [SYSTEM].
+ This file is part of G.A. Library.
 
- [SYSTEM] is free software; you can redistribute it and/or modify it
- under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+ G.A. Library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
 
- [SYSTEM] is distributed in the hope that it will be useful, but
+ G.A. Library is distributed in the hope that it will be useful, but
  WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with [SYSTEM]; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ along with G.A. Library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
 
  System: G.A. Lib
 
@@ -24,33 +25,31 @@
 
  CVS Information
  $Author: ron_lima $
- $Id: dlist_alloc.c,v 1.7 2004-10-05 10:29:28 ron_lima Exp $
+ $Id: dlist_alloc.c,v 1.8 2004-10-13 00:29:12 ron_lima Exp $
 */
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "dlist.h"
 
 int
-dlist_alloc(dlist_t ** list, deallocator_t * dealloc)
+dlist_alloc (dlist_t ** list, deallocator_t * dealloc)
 {
   /* Assertives for debugging purposes */
-  assert(list != NULL);
-  assert(dealloc != NULL);
+  assert (list != NULL);
+  assert (dealloc != NULL);
+  
   /* The deallocator must be always provided */
-  if (!dealloc)
+  if (! dealloc)
   {
-    errno = EINVAL;
-    return -1;
+    return EGAINVAL;
   }
   /* Allocates memory for the list structure */
-  *list = (dlist_t *) malloc(sizeof(dlist_t));
-  assert(*list != NULL);
-  if (!*list)
+  *list = (dlist_t *) malloc (sizeof (dlist_t));
+  assert (*list != NULL);
+  if (! *list)
   {
-    errno = ENOMEM;
-    return -1;
+    return EGANOMEM;
   }
   /* Initializes each data member of the list descriptor */
   (*list)->size_ = 0x0;
