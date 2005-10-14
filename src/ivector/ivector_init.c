952509a9c1e06ef717e57a3f742c02145e9799b0
ron_lima <ron_lima@675ac745-d94a-0410-b520-95f516b70467>
2005-10-14 09:58:23 +0000

    T119975:
    Deleted the deallocator parameter.
    Made some aesthetic changes to increase readability.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@236 675ac745-d94a-0410-b520-95f516b70467

--- src/ivector/ivector_init.c
+++ src/ivector/ivector_init.c
@@ -24,7 +24,7 @@
 
  CVS Information
  $Author: ron_lima $
- $Id: ivector_init.c,v 1.3 2005-08-29 10:42:37 ron_lima Exp $
+ $Id: ivector_init.c,v 1.4 2005-10-14 09:58:23 ron_lima Exp $
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,17 +33,16 @@
 #include "ivector.h"
 
 /* Version info */
-static char const rcsid [] = "@(#) $Id: ivector_init.c,v 1.3 2005-08-29 10:42:37 ron_lima Exp $";
+static char const rcsid [] = "@(#) $Id: ivector_init.c,v 1.4 2005-10-14 09:58:23 ron_lima Exp $";
 
 int
-ivector_init (ivector_t * vector, compare_t * comp, deallocator_t * dealloc,
-              size_t datalen)
+ivector_init (ivector_t * vector, compare_t * comp, size_t datalen)
 {
   assert (vector != NULL);
   assert (comp != NULL);
   assert (datalen != 0);
 
-  if (!datalen)
+  if (datalen == 0x0)
     {
       return EGAINVAL;
     }
@@ -53,7 +52,6 @@ ivector_init (ivector_t * vector, compare_t * comp, deallocator_t * dealloc,
   vector->elemused_   = 0x0;
   vector->chunksused_ = 0x0;
   vector->comp_       = comp;
-  vector->dealloc_    = dealloc;
   vector->data_       = (void *) NULL;
   vector->signature_  = GA_IVECTOR_SIGNATURE;
 
