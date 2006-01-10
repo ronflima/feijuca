4ab292fed28c8c91ea5e7cdbe9cc435ccf2bbb33
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2006-01-10 09:47:07 +0000

    F1382897:
    - Added the implementation of the function dclist_size
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@280 675ac745-d94a-0410-b520-95f516b70467

---
+++ src/dclist/dclist_size.c
@@ -0,0 +1,51 @@
+/* -*-c-*-
+ G.A. Library - A generic algorithms and data structures library
+ Copyright (C) 2005 - Ronaldo Faria Lima
+
+ This is contributed code by Daniel Costa Soares
+ <daniel_csoares@yahoo.com.br>
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ USA
+
+ System: G.A. Lib
+
+ Description: Determines the size of a double linked-list
+
+ CVS Information
+ $Author: harq_al_ada $
+ $Id: dclist_size.c,v 1.1 2006-01-10 09:47:07 harq_al_ada Exp $
+*/
+#include <stddef.h>
+#include <assert.h>
+#include "dclist.h"
+#include "dlist.h"
+#include "gacommon.h"
+
+/* Version info */
+static char const rcsid[] = "@(#) $Id: dclist_size.c,v 1.1 2006-01-10 09:47:07 harq_al_ada Exp $";
+
+int
+dclist_size (dclist_t * dclist, size_t * size)
+{
+  assert (dclist != NULL);
+  assert (size != NULL);
+  if (dclist == NULL || size == NULL)
+    {
+      return EGAINVAL;
+    }
+  CHECK_SIGNATURE (dclist, GA_DCLIST_SIGNATURE);
+  return dlist_size (&dclist->list_, size);
+}
