426760ebec931d132a86bb3fd2790f4d79d0cf70
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2006-01-10 09:49:41 +0000

    F1382897:
    - Added the implementation of the funcion deque_size
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@282 675ac745-d94a-0410-b520-95f516b70467

---
+++ src/deque/deque_size.c
@@ -0,0 +1,50 @@
+/* -*-c-*-
+ G.A. Library - A generic algorithms and data structures library
+ Copyright (C) 2005 - Ronaldo Faria Lima
+
+ This is contributed code by Daniel Costa Soares
+ <daniel_csoares@yahoo.com.br>
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ USA
+
+ System: G.A. Lib
+
+ Description: Determines the size of a deque
+
+ CVS Information
+ $Author: harq_al_ada $
+ $Id: deque_size.c,v 1.1 2006-01-10 09:49:41 harq_al_ada Exp $
+*/
+#include <stddef.h>
+#include <assert.h>
+#include "deque.h"
+#include "dlist.h"
+
+/* Version info */
+static char const rcsid[] = "@(#) $Id: deque_size.c,v 1.1 2006-01-10 09:49:41 harq_al_ada Exp $";
+
+int
+deque_size (deque_t * deque, size_t * size)
+{
+  assert (deque != NULL);
+  assert (size != NULL);
+  if (deque == NULL || size == NULL)
+    {
+      return EGAINVAL;
+    }
+  CHECK_SIGNATURE (deque, GA_DEQUE_SIGNATURE);
+  return dlist_size (&deque->list_, size);
+}
