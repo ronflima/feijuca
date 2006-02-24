329108ed744ceff3ad145fec7a5d8fed2f42f5ac
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2006-02-24 10:48:08 +0000

    Added the list element constructor
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@367 675ac745-d94a-0410-b520-95f516b70467

---
+++ src/list/elements/list_element_init_.c
@@ -0,0 +1,62 @@
+/* -*-c-*-
+
+ Copyright Ronaldo Faria Lima (C) 2004 - All rights reserved
+
+ Feel free to contact the author in <ronaldo@ronaldolima.eti.br>
+
+ This file is part of G.A. Lib.
+
+ G.A. Lib is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ G.A. Lib is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with G.A. Lib; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  System: G.A. Lib
+
+  Description: Creates a list element
+
+ CVS Information $Author: harq_al_ada $ $Id: list_del.c,v 1.7 2004/07/19 00:53:45
+ ron_lima Exp $
+ */
+#include <assert.h>
+#include "list_.h"
+
+/* Version info */
+static char const rcsid [] = "@(#) $Id: list_element_init_.c,v 1.1 2006-02-24 10:48:08 harq_al_ada Exp $";
+
+int
+list_element_init_ (list_element_t * element, const void * data, deallocator_t *dealloc)
+{
+  int rc = 0x0;
+
+  assert (element != NULL);
+  assert (dealloc != NULL);
+  if ((element == NULL) || (dealloc == NULL))
+    {
+      rc = EGAINVAL;
+    }
+  else 
+    {
+      if ((*element = (list_element_t) malloc (sizeof (struct list_element_t))) == NULL)
+        {
+          rc = EGANOMEM;
+        }
+      else
+        {
+          (*element)->next_        = NULL;
+          (*element)->data_        = data;
+          (*element)->deallocator_ = dealloc;
+        }
+    }
+  
+  return rc;
+}
