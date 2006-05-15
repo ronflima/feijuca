ed4e26d3b1f14090707baacfd90613cbe79ecbbd
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2006-05-15 10:54:55 +0000

    Changed the call to the list handlers: the first parameter was wrong.
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@387 675ac745-d94a-0410-b520-95f516b70467

--- src/list/list_init.c
+++ src/list/list_init.c
@@ -24,14 +24,14 @@
 
  CVS Information
  $Author: harq_al_ada $
- $Id: list_init.c,v 1.8 2006-05-14 18:27:07 harq_al_ada Exp $
+ $Id: list_init.c,v 1.9 2006-05-15 10:54:55 harq_al_ada Exp $
 */
 #include <assert.h>
 #include "list.h"
 #include "list_.h"
 
 /* Version info */
-static char const rcsid [] = "@(#) $Id: list_init.c,v 1.8 2006-05-14 18:27:07 harq_al_ada Exp $";
+static char const rcsid [] = "@(#) $Id: list_init.c,v 1.9 2006-05-15 10:54:55 harq_al_ada Exp $";
 
 GAERROR
 list_init (list_t * list, deallocator_t * dealloc)
@@ -49,10 +49,10 @@ list_init (list_t * list, deallocator_t * dealloc)
       return EGANOMEM;
     }
   memset(list, 0x0, sizeof(struct list_t));
-  list_set_head_(list, NULL);
-  list_set_tail_(list, NULL);
-  list_set_curr_(list, NULL);
-  list_set_deallocator_(list, dealloc);
-  list_set_signature_(list);
+  list_set_head_(*list, NULL);
+  list_set_tail_(*list, NULL);
+  list_set_curr_(*list, NULL);
+  list_set_deallocator_(*list, dealloc);
+  list_set_signature_(*list);
   return EGAOK;
 }
