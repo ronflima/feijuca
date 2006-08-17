2e9302ccbeef7975342e1fbf12200aaf25ad7623
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2006-08-17 11:14:17 +0000

    Deleted wrong assertives.
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@425 675ac745-d94a-0410-b520-95f516b70467

--- src/list/elements/list_element_get_data_.c
+++ src/list/elements/list_element_get_data_.c
@@ -32,14 +32,11 @@
 #include "list_.h"
 
 /* Version info */
-static char const rcsid [] = "@(#) $Id: list_element_get_data_.c,v 1.3 2006-05-15 10:56:21 harq_al_ada Exp $";
+static char const rcsid [] = "@(#) $Id: list_element_get_data_.c,v 1.4 2006-08-17 11:14:17 harq_al_ada Exp $";
 
 GAERROR
 list_element_get_data_ (list_element_t element, void ** data)
 {
-  assert (element != NULL);
-  assert (data != NULL);
-
   if ((element == NULL) || (data == NULL))
     {
       return EGAINVAL;
