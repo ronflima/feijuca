133dabde42cef49f66098865fd7fbf22ebef7bab
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2006-08-19 13:23:20 +0000

    Undone last changes. They were equivocated.
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@426 675ac745-d94a-0410-b520-95f516b70467

--- src/testsuite/gatests.h
+++ src/testsuite/gatests.h
@@ -23,7 +23,7 @@
 
  CVS Information
  $Author: harq_al_ada $
- $Id: gatests.h,v 1.17 2006-08-14 00:16:24 harq_al_ada Exp $
+ $Id: gatests.h,v 1.18 2006-08-19 13:23:20 harq_al_ada Exp $
 */
 
 #ifndef GATESTS_H
@@ -58,8 +58,8 @@ GABEGINDECLS
  * Datatypes
  */
 /* Test procedures data types */
-typedef GAERROR (test_proc_t) (size_t);	    /* Test procedure */
-typedef GAERROR (test_scenario_t) (size_t); /* Test scenario  */
+typedef int (test_proc_t) (size_t);	/* Test procedure */
+typedef int (test_scenario_t) (size_t);	/* Test scenario  */
 
 typedef struct
 {
@@ -92,15 +92,15 @@ cmdline_t;			/* Command line descriptor */
 /*
  * Prototypes
  */
-GAERROR (test_list) (size_t);
-GAERROR (test_dlist) (size_t);
-GAERROR (test_stack) (size_t);
-GAERROR (test_clist) (size_t);
-GAERROR (test_queue) (size_t);
-GAERROR (test_ivector) (size_t);
-GAERROR (test_deque) (size_t);
-GAERROR (test_dclist) (size_t);
-GAERROR (execute_scenarios) (const char *, size_t, scenario_t[], size_t);
+int (test_list) (size_t);
+int (test_dlist) (size_t);
+int (test_stack) (size_t);
+int (test_clist) (size_t);
+int (test_queue) (size_t);
+int (test_ivector) (size_t);
+int (test_deque) (size_t);
+int (test_dclist) (size_t);
+int (execute_scenarios) (const char *, size_t, scenario_t[], size_t);
 
 GAENDDECLS
 #endif /* GATESTS_H */
