cdfd322bdaf3ff99daca39a6eaab6df3a1a1dbc9
harq_al_ada <harq_al_ada@675ac745-d94a-0410-b520-95f516b70467>
2007-02-25 13:03:48 +0000

    Changed return codes to comply with the new error codification from the library
    git-svn-id: svn+ssh://svn.code.sf.net/p/feijuca/code/trunk@436 675ac745-d94a-0410-b520-95f516b70467

--- src/testsuite/list.c
+++ src/testsuite/list.c
@@ -24,7 +24,7 @@
 
  CVS Information
  $Author: harq_al_ada $
- $Id: list.c,v 1.27 2006-06-12 10:05:29 harq_al_ada Exp $
+ $Id: list.c,v 1.28 2007-02-25 13:03:48 harq_al_ada Exp $
 */
 
 #include <stdio.h>
@@ -34,7 +34,7 @@
 #include "list.h"
 
 /* Version info */
-static char const rcsid [] = "@(#) $Id: list.c,v 1.27 2006-06-12 10:05:29 harq_al_ada Exp $";
+static char const rcsid [] = "@(#) $Id: list.c,v 1.28 2007-02-25 13:03:48 harq_al_ada Exp $";
 
 /*
  * Local macros
@@ -55,6 +55,10 @@ enum
 static int load_list (list_t, size_t, unsigned char);
 static int check_contents (size_t);
 static int check_deletion (size_t);
+static GAERROR check_deletion_in_middle (list_t, int *);
+static GAERROR check_deletion_in_head (list_t, int *);
+static GAERROR check_deletion_in_tail (list_t);
+static GAERROR check_deletion_near_tail (list_t, int *);
 static int check_reversal (size_t);
 
 int
@@ -114,8 +118,7 @@ load_list (list_t list, size_t elements, unsigned char use_pattern)
 static int
 check_contents (size_t elements)
 {
-  int *item;			/* Item to grab from the list */
-  int rc;			/* General error checking variable */
+  GAERROR rc;			/* General error checking variable */
   int test_status;		/* Test status */
   register int i;		/* General iterator */
   list_t list;
@@ -123,7 +126,7 @@ check_contents (size_t elements)
   test_status = 0x0;
   i = 0x0;
 
-  if ((rc = list_init (&list, free)) != 0x0)
+  if ((rc = list_init (&list, free)) != EGAOK)
     {
       ERROR (TEST, "list_init", rc);
       return rc;
@@ -133,14 +136,16 @@ check_contents (size_t elements)
       ERROR (TEST, "load_list", rc);
       test_status = EFAILED;
     }
-  else if ((rc = list_move (list, POS_HEAD)) != 0x0)
+  else if ((rc = list_move (list, POS_HEAD)) != EGAOK)
     {
       ERROR (TEST, "list_move", rc);
       test_status = EFAILED;
     }
   while (test_status == 0x0)
     {
-      if ((rc = list_get (list, (void **) &item, POS_CURR)) != 0x0)
+      int *item;			/* Item to grab from the list */
+
+      if ((rc = list_get (list, (void **) &item, POS_CURR)) != EGAOK)
         {
           ERROR (TEST, "list_get", rc);
           test_status = EFAILED;
@@ -153,7 +158,7 @@ check_contents (size_t elements)
           break;
         }
       ++i;
-      if ((rc = list_move (list, POS_NEXT)) == EOF)
+      if ((rc = list_move (list, POS_NEXT)) == EGAEOF)
         {
           break;
         }
@@ -169,7 +174,7 @@ check_contents (size_t elements)
       ERROR (TEST, "Data number mismatch in data retrieval", ECKFAIL);
       test_status = EFAILED;
     }
-  if ((rc = list_destroy (list)) != 0x0)
+  if ((rc = list_destroy (list)) != EGAOK)
     {
       ERROR (TEST, "list_destroy", ECKFAIL);
       test_status = EFAILED;
@@ -184,107 +189,213 @@ check_contents (size_t elements)
 static int
 check_deletion (size_t elements)
 {
-  int deleted = 0x0;            /* Number of elements deleted */
-  int *item;			/* Item deleted from the list */
-  int rc;			/* General purpose error handling variable */
+  GAERROR rc;                   /* General purpose error handling variable */
   int test_status = 0x0;        /* Test status */
   register int i = 0x0;		/* A simple iterator */
-  list_t list;
+  list_t list;                  /* List descriptor */
+  int nelem = 0x0;              /* Number of elements */
+  int ndeleted = 0x0;           /* Number of deleted elements */
 
-  if ((rc = list_init (&list, free)) != 0x0)
+  if ((rc = list_init (&list, free)) != EGAOK)
     {
       ERROR (TEST, "list_init", rc);
       test_status = EFAILED;
     }
-  else if ((rc = load_list (list, elements, '\x1')) != 0x0)
+  else if ((rc = load_list (list, elements, '\x1')) != EGAOK)
     {
       ERROR (TEST, "load_list", rc);
       test_status = EFAILED;
     }
-  else if ((rc = list_move (list, POS_HEAD)) != 0x0)
+  else 
     {
-      ERROR (TEST, "list_move", rc);
-      test_status = EFAILED;
-    }
-  else if ((rc = list_del (list, (void **) &item, POS_HEAD)) != 0x0)
-    {
-      ERROR (TEST, "list_del", rc);
-      test_status = EFAILED;
-    }
-  else
-    {
-      free (item);
-      ++deleted;
+      size_t size;              /* Size of the list after deletions */
 
-      if ((rc = list_move (list, POS_HEAD)) != 0x0)
+      if ((rc = list_get_size (list, &nelem)) != EGAOK)
+        {
+          ERROR (TEST, "list_get_size", rc);
+          test_status = EFAILED;
+        }
+      else if ((rc = list_move (list, POS_HEAD)) != EGAOK)
         {
           ERROR (TEST, "list_move", rc);
           test_status = EFAILED;
         }
-      else
+      else if ((rc = check_deletion_near_tail (list, &ndeleted)) != EGAOK)
         {
-          register int i = 0x0;
-          size_t list_length;
-          
-          if ((rc = list_get_size (list, &list_length)) != 0x0)
-            {
-              ERROR (TEST, "list_get_size", rc);
-              test_status = EFAILED;
-            }
-          else
-            {
-              while ((rc = list_move (list, POS_NEXT)) == 0x0)
-                {
-                  ++i;
-                  if (i == (list_length - 2))
-                    {
-                      break;
-                    }
-                }
-              if (rc != EOF && rc != 0x0)
-                {
-                  ERROR (TEST, "list_move", rc);
-                  test_status = EFAILED;               
-                }
-              else if ((rc = list_del (list, NULL, POS_NEXT)) == 0x0)
-                {
-                  ++deleted;
-                  if ((rc = list_get_size (list, &list_length)) != 0x0)
-                    {
-                      ERROR (TEST, "list_get_size", rc);
-                      test_status = EFAILED;
-                    }
-                  else if (list_length != (elements - deleted))
-                    {
-                      ERROR (TEST, "list length mismatch", ECKFAIL);
-                      test_status = EFAILED;
-                    }
-                  else if ((rc = list_move (list, POS_HEAD)) != 0x0)
-                    {
-                      ERROR (TEST, "list_move", rc);
-                      test_status = EFAILED;
-                    }
-                  else if ((rc = list_del (list, NULL, POS_NEXT)) != 0x0)
-                    {
-                      ERROR (TEST, "list_del", rc);
-                      test_status = EFAILED;
-                    }
-                }
-              else
-                {
-                  ERROR (TEST, "list_del", rc);
-                  test_status = EFAILED;
-                  
-                }
-            }
+          ERROR (TEST, "check_deletion_near_tail", ECKFAIL);
+          test_status = EFAILED;
+        }
+      else if ((rc = check_deletion_in_middle (list, &ndeleted)) != EGAOK)
+        {
+          ERROR (TEST, "check_deletion_in_middle", ECKFAIL);
+          test_status = EFAILED;
+        }
+      else if ((rc = check_deletion_in_head (list, &ndeleted)) != EGAOK)
+        {
+          ERROR (TEST, "check_deletion_in_head", ECKFAIL);
+          test_status = EFAILED;
+        }
+      else if ((rc = check_deletion_in_tail (list)) != EGAOK)
+        {
+          ERROR (TEST, "check_deletion_in_tail", ECKFAIL);
+          test_status = EFAILED;
+        }
+      else if ((rc = list_get_size (list, &size)) != EGAOK)
+        {
+          ERROR (TEST, "list_get_size", rc);
+          test_status = EFAILED;
+        }
+      else if ((nelem - ndeleted) != size)
+        {
+          ERROR (TEST, "List number of items mismatch", ECKFAIL);
+          test_status = EFAILED;
         }
     }
-  if ((rc = list_destroy (list)) != 0x0)
+  if ((rc = list_destroy (list)) != EGAOK)
     {
       ERROR (TEST, "list_destroy", ECKFAIL);
       test_status = EFAILED;
     }
   return test_status;
+}
+/*
+ * Part of the deletion test scenario: tests deletion at the middle of
+ * the list.
+ */
+static GAERROR check_deletion_in_middle(list_t list, int * deleted)
+{
+  GAERROR rc;                   /* Error handling */
+  size_t size;                  /* Size of the list */
+
+  if ((rc = list_move (list, POS_HEAD)) != EGAOK)
+    {
+      ERROR (TEST, "list_move", rc);
+    }
+  else if ((rc = list_get_size (list, &size)) != EGAOK)
+    {
+      ERROR (TEST, "list_get_size", rc);
+    }
+  else
+    {
+      register int i;
+
+      /* Move the curr to the middle of the list */
+      for (i=0x0; i<size/2; ++i) 
+        {
+          if ((rc == list_move (list, POS_NEXT)) != EGAOK)
+            {
+              if (rc > 0x0) 
+                {
+                  ERROR (TEST, "list_move", rc);
+                }
+              break;
+            }
+        }
+      if (rc == EGAOK)
+        {
+          if ((rc = list_del (list, NULL, POS_NEXT)) != EGAOK)
+            {
+              ERROR (TEST, "list_del", rc);
+            }
+          else 
+            {
+              ++(*deleted);
+            }
+        }
+    }
+  return rc;
+}
+/*
+ * Part of the deletion test scenario: Check deletions on the head of
+ * the list.
+ */
+static GAERROR check_deletion_in_head (list_t list, int * deleted)
+{
+  GAERROR rc;                   /* Error handling */
+
+  if ((rc = list_del (list, NULL, POS_HEAD)) != EGAOK)
+    {
+      ERROR (TEST, "list_del", rc);
+    }
+  else
+    {
+      ++(*deleted);
+    }
+  return rc;
+}
+/*
+ * Part of the deletion test scenario: Check deletions on the element
+ * prior to the tail
+ */
+static GAERROR check_deletion_near_tail (list_t list, int * ndeleted)
+{
+  GAERROR rc;                   /* Error handling */
+  size_t size;                  /* Size of the list */
+
+  if ((rc = list_get_size (list, &size)) != EGAOK)
+    {
+      ERROR (TEST, "list_get_size", rc);
+    }
+  else 
+    {
+      if (size > 1)
+        {
+          if ((rc = list_move (list, POS_HEAD)) != EGAOK)
+            {
+              ERROR (TEST, "list_move", rc);
+            }
+          else
+            {
+              register int i = size;     /* Counter to navigate the list */
+
+              /* This test makes sense only if the list has size greater
+               * than 1 */
+              while (i - 2 > 0)
+                {
+                  if ((rc = list_move (list, POS_NEXT)) != EGAOK)
+                    {
+                      break;
+                    }
+                  --i;
+                }
+              if (rc == EGAOK)
+                {
+                  if ((rc = list_del (list, NULL, POS_NEXT)) != EGAOK)
+                    {
+                      ERROR (TEST, "list_del", rc);
+                    }
+                  else
+                    {
+                      ++(*ndeleted);
+                    }
+                }
+              else if (rc != EGAEOF)
+                {
+                  ERROR (TEST, "list_move", rc);
+                }
+            }
+        }
+    }
+  return rc;
+}
+/*
+ * Part of the deletion test scenario: Check deletions on the tail of
+ * the list - Exception since it is not possible to delete anything
+ * from the tail.
+ */
+static GAERROR check_deletion_in_tail (list_t list)
+{
+  GAERROR rc;                   /* Error handling */
+
+  if ((rc = list_del (list, NULL, POS_TAIL)) == EGAOK)
+    {
+      ERROR (TEST, "list_del", ECKFAIL);
+    }
+  else
+    {
+      rc = EGAOK;
+    }
+  return rc;
 }
 
 /*
@@ -293,13 +404,13 @@ check_deletion (size_t elements)
 static int
 check_reversal (size_t elements)
 {
-  int rc;                       /* General return code */
+  GAERROR rc;                   /* General return code */
   int bigger = 0x0;             /* Bigger item */
   size_t curr_item = 0x0u;      /* Current item counter */
   list_t list;                 
   int test_status = 0x0;
 
-  if ((rc = list_init (&list, free)) != 0x0)
+  if ((rc = list_init (&list, free)) != EGAOK)
     {
       ERROR (TEST, "list_init", rc);
       return rc;
@@ -309,14 +420,14 @@ check_reversal (size_t elements)
       ERROR (TEST, "load_list", rc);
       test_status = EFAILED;
     }
-  else if ((rc = list_reverse (list)) != 0x0)
+  else if ((rc = list_reverse (list)) != EGAOK)
     {
       ERROR (TEST, "Error reversing the list", rc);
       test_status = EFAILED;
     }
   else 
     {
-      if ((rc = list_move (list, POS_HEAD)) != 0x0)
+      if ((rc = list_move (list, POS_HEAD)) != EGAOK)
         {
           ERROR (TEST, "list_move", rc);
           test_status = EFAILED;
@@ -325,7 +436,7 @@ check_reversal (size_t elements)
   while (test_status == 0x0)
     {
       int * data;
-      if ((rc = list_get (list, (void **) &data, POS_NEXT)) == EOF)
+      if ((rc = list_get (list, (void **) &data, POS_NEXT)) == EGAEOF)
         {
           break;
         }
@@ -350,10 +461,11 @@ check_reversal (size_t elements)
           test_status = EFAILED;
         }
     }
-  if ((rc = list_destroy (list)) != 0x0)
+  if ((rc = list_destroy (list)) != EGAOK)
     {
       ERROR (TEST, "list_destroy", ECKFAIL);
       test_status = EFAILED;
     }
   return test_status;
 }
+
